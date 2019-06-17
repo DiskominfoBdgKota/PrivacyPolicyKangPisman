e7df6cb69299c387b2f5e5a9c425bfa19a1f7b99
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-06-17 16:16:27 +0700

    Kang Pisman Privacy Policy Deploy

---
+++ precache-manifest.34a29db5cca06badc5f729074799c224.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8489402ae4353e23a9cf",
+    "url": "/PrivacyPolicyKangPismanApp/js/privacy-page-legacy.859af7fa.js"
+  },
+  {
+    "revision": "b2069bdaeb6420b01fec",
+    "url": "/PrivacyPolicyKangPismanApp/js/chunk-vendors-legacy.6c31ac47.js"
+  },
+  {
+    "revision": "fe92300fa6d0e6f55c24",
+    "url": "/PrivacyPolicyKangPismanApp/js/app-legacy.a5ae89a6.js"
+  },
+  {
+    "revision": "443e463a86bf1973504eda539ab83fec",
+    "url": "/PrivacyPolicyKangPismanApp/index.html"
+  },
+  {
+    "revision": "a373d8cb3c10bf6223f30546b5f1cf58",
+    "url": "/PrivacyPolicyKangPismanApp/img/diskominfo_kota.a373d8cb.png"
+  },
+  {
+    "revision": "8489402ae4353e23a9cf",
+    "url": "/PrivacyPolicyKangPismanApp/css/privacy-page.c25ed838.css"
+  },
+  {
+    "revision": "b2069bdaeb6420b01fec",
+    "url": "/PrivacyPolicyKangPismanApp/css/chunk-vendors.11cef110.css"
+  },
+  {
+    "revision": "fe92300fa6d0e6f55c24",
+    "url": "/PrivacyPolicyKangPismanApp/css/app.0e433876.css"
+  }
+];
---
+++ precache-manifest.6b39628538772ae2ee1110c1752890be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PrivacyPolicyKangPismanApp/robots.txt"
+  },
+  {
+    "revision": "392e2cb60f0f09370080",
+    "url": "/PrivacyPolicyKangPismanApp/js/privacy-page.393f1e6d.js"
+  },
+  {
+    "revision": "3181597b6e803766be30",
+    "url": "/PrivacyPolicyKangPismanApp/js/chunk-vendors.a3a97098.js"
+  },
+  {
+    "revision": "7018f64f7323c7f2d6cd",
+    "url": "/PrivacyPolicyKangPismanApp/js/app.16cbe42b.js"
+  },
+  {
+    "revision": "f42487c09b96f8bd63a2f59f739234ca",
+    "url": "/PrivacyPolicyKangPismanApp/index.html"
+  },
+  {
+    "revision": "a373d8cb3c10bf6223f30546b5f1cf58",
+    "url": "/PrivacyPolicyKangPismanApp/img/diskominfo_kota.a373d8cb.png"
+  },
+  {
+    "revision": "392e2cb60f0f09370080",
+    "url": "/PrivacyPolicyKangPismanApp/css/privacy-page.c25ed838.css"
+  },
+  {
+    "revision": "3181597b6e803766be30",
+    "url": "/PrivacyPolicyKangPismanApp/css/chunk-vendors.11cef110.css"
+  },
+  {
+    "revision": "7018f64f7323c7f2d6cd",
+    "url": "/PrivacyPolicyKangPismanApp/css/app.0e433876.css"
+  }
+];
